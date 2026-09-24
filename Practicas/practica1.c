05fec765840236a44ddf30c96ac212ccebdb38cd
Joeltr000 <joelstudy00@gmail.com>
2026-09-23 23:15:04 -0600

    Arreglo bidimensional, Funcion con paso por valor

--- Practicas/practica1.c
+++ Practicas/practica1.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 
+int obtenerColumna(char evento){
+    if (evento == 'C') return 0;
+    if (evento == 'I') return 1;
+    if (evento == 'N') return 2;
+    if (evento == 'B') return 3;
+    return -1;
+}
+
 int main(){
 
     int equipos[6] = {0, 1, 1, 1, 1, 0};
     char eventos[6] = {'C', 'I', 'I', 'I', 'N', 'C'}; //'C'= Acceso correcto,'I'= Contraseña incorrecta,'N'= Usuario inexistente,'B'= Acceso bloqueado.
 
+    int matriz[2][4] = {0};
+
     printf("__Registro de eventos__\n");
     for(int i=0; i<6; i++) {
 	printf("Evento %d | Equipo: PC-0%d | Tipo: %c\n", i + 1, equipos[i] + 1, eventos[i]);
     }
+    printf("\n__Mapping de Columnas__\n");
+    for (int i=0; i<6; i++){
+        int col = obtenerColumna(eventos[i]);
+        printf("El evento '%c' se guarda en la columna; %d\n", eventos[i], col);
+    } 
 
     return 0;
 }

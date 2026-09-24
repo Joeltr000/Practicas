eeaede0635a872a10f6795227105075ca2d36e12
Joeltr000 <joelstudy00@gmail.com>
2026-09-23 22:38:52 -0600

    Arreglos unidimencionales

--- Practicas/practica1.c
+++ Practicas/practica1.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
 
+int main(){
+
+    int equipos[6] = {0, 1, 1, 1, 1, 0};
+    char eventos[6] = {'C', 'I', 'I', 'I', 'N', 'C'}; //'C'= Acceso correcto,'I'= Contraseña incorrecta,'N'= Usuario inexistente,'B'= Acceso bloqueado.
+
+    printf("__Registro de eventos__\n");
+    for(int i=0; i<6; i++) {
+	printf("Evento %d | Equipo: PC-0%d | Tipo: %c\n", i + 1, equipos[i] + 1, eventos[i]);
+    }
+
+    return 0;
+}
